8273027cdc741435a7649d3c887bdb69ddcef1af
korkutemir <55914306+korkutemir@users.noreply.github.com>
2023-01-20 00:49:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+require("dotenv").config();
+const express = require('express');
+const app = express();
+var port=process.env.PORT || 3000;
+
+app.get('/', function (req, res) {
+
+  res.send('Merhaba Express');
+  res.send('<h1>Merhaba Express</h1>');
+ // res.sendFile( __dirname + "/" + "earth.html");
+
+});
+
+app.post('/', function (req, res) {
+ res.send('Merhaba Express');
+ // res.sendFile( __dirname + "/" + "earth.html");
+});
+
+app.listen(port, function () {
+  console.log('Sunucu çalışıyor...');
+});
