2c6cd8317a848eb9be224dd0118496ed214289bc
korkutemir <55914306+korkutemir@users.noreply.github.com>
2023-01-20 01:52:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var port=process.env.PORT || 3000;
 
 //app.use(express.static(path.join(__dirname, "js")));
 
+app.use(express.static('public'));
+
 app.get('/', function (req, res) {
 
   //res.send('Merhaba Express');
